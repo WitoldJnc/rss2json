943209dea0a8cf80bcab129af0be4f68e8a51173
anton <witold.jnc@gmail.com>
2020-04-05 13:58:59 +0500

    create empty repo

---
+++ src/main/java/com/rss2json/rss2json/Rss2jsonApplication.java
@@ -0,0 +1,13 @@
+package com.rss2json.rss2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rss2jsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rss2jsonApplication.class, args);
+	}
+
+}
